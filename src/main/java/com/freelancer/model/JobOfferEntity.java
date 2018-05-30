562953cd1a1aff8675ac22e1963158e26cf0a3f9
Jean Oliveira Gomes <jeanoliveiragomes1@hotmail.com>
2018-05-30 20:01:02 -0300

    relacionado entidades JobOfferEntity e UserEntity

--- src/main/java/com/freelancer/model/JobOfferEntity.java
+++ src/main/java/com/freelancer/model/JobOfferEntity.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class JobOfferEntity {
@@ -11,8 +12,8 @@ public class JobOfferEntity {
 	@Id
 	@GeneratedValue( strategy=GenerationType.AUTO )
 	private Integer id;
-	private String description;
-	private String title;
-	private long precoOferecido;
+	
+	@ManyToOne
+	private UserEntity owner;
 	
 }
--- src/main/java/com/freelancer/model/UserEntity.java
+++ src/main/java/com/freelancer/model/UserEntity.java
@@ -1,9 +1,14 @@
 package com.freelancer.model;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class UserEntity {
@@ -11,49 +16,16 @@ public class UserEntity {
 	@Id
 	@GeneratedValue( strategy=GenerationType.AUTO )
 	private Integer id;
+	
+	@Column(length=20, nullable=false, unique=true)
+	private String nickname;
+	
+	@Column(length=50, nullable=false)
 	private String nome;
-	private String email;
-	private String cpf;
-	//2 = usuario/publicador 1= administrador type enum
-	private int avaliacao;	
-	private int nivelAcesso;
 	
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public int getAvaliacao() {
-		return avaliacao;
-	}
-	public void setAvaliacao(int avaliacao) {
-		this.avaliacao = avaliacao;
-	}
-	public int getNivelAcesso() {
-		return nivelAcesso;
-	}
-	public void setNivelAcesso(int nivelAcesso) {
-		this.nivelAcesso = nivelAcesso;
-	}
+	private Date datanascimento;
 	
+	 @OneToMany( targetEntity=JobOfferEntity.class )
+	private List<JobOfferEntity> ofertasVagas;
 
 }
