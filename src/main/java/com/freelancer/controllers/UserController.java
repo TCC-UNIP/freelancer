f973a1c22f58748fcecc0c1287a15eeb8f3ca568
unknown <jeanoliveiragomes1@hotmail.com>
2018-05-30 11:59:13 -0300

    implementando UserController e UserServices

--- src/main/java/com/freelancer/controllers/UserController.java
+++ src/main/java/com/freelancer/controllers/UserController.java
@@ -1,5 +1,9 @@
 package com.freelancer.controllers;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,21 +16,31 @@ import com.freelancer.model.UserEntity;
 import com.freelancer.services.UserServices;
 
 @Controller
-@RequestMapping(value="cliente")
+@RequestMapping(value="/cliente")
 public class UserController {
 	
 	@Autowired
 	UserServices userServ;
 	
 	
-	@RequestMapping(method= RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(method= RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseBody
 	public  UserEntity salvar(@RequestBody UserEntity user) {
 		
 		return userServ.saveAndUpdate(user) ;
-		   
 	}
 	
 	
+	@RequestMapping(value="/all", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public  List<UserEntity> listar() {
+		return userServ.listAllUsers() ;
+	}
+	
+	@RequestMapping(value="/buscar{id}", method= RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public  Optional<UserEntity> findbyid(@Param("id") Integer id) {
+		return userServ.findbyid(id) ;
+	}
 	
 }
--- src/main/java/com/freelancer/services/UserServices.java
+++ src/main/java/com/freelancer/services/UserServices.java
@@ -1,5 +1,8 @@
 package com.freelancer.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,19 @@ public class UserServices {
 		return userRepository.save(user);
 	}
 	
+	public void delete(Integer id) {
+		userRepository.deleteById(id);
+	}
+
 	public void delete(UserEntity user) {
 		userRepository.delete(user);
 	}
 	
+	public List<UserEntity> listAllUsers(){
+		return userRepository.findAll();
+	}
+	
+	public Optional<UserEntity> findbyid(Integer id) {
+		return userRepository.findById(id);		
+	}
 }
