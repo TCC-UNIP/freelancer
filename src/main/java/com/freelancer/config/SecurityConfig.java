6691cac42e0415d009c73a39a5faf078c189068b
unknown <jeanoliveiragomes1@hotmail.com>
2018-06-15 13:29:33 -0300

    spring security finalizado

--- src/main/java/com/freelancer/config/SecurityConfig.java
+++ src/main/java/com/freelancer/config/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated();
 		http.authorizeRequests().antMatchers("*/admim/*").hasRole("ADMIN").antMatchers("*/protected/*").hasRole("USER").and().httpBasic().and().csrf().disable();
 	}
 	
--- src/main/java/com/freelancer/controllers/UserController.java
+++ src/main/java/com/freelancer/controllers/UserController.java
@@ -6,12 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +27,7 @@ public class UserController {
 	UserServices userServ;
 	
 	//SAVAR USUARIO
-	@PutMapping(value="/protected/user")
+	@PutMapping(value="/user")
 	@ResponseBody
 	public UserEntity save(@RequestBody UserEntity user) {
 		user.setPassword(userServ.encpritografarBcripty(user.getPassword()));
